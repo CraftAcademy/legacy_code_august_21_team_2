2d43a9181b8234473904d806a230aacfe5e5c937
johnnysee <js.johnny.see@gmail.com>
2021-09-23 12:11:36 +0200

    added fixtures folder files, define params

--- spec/requests/api/images/index_spec.rb
+++ spec/requests/api/images/index_spec.rb
@@ -1,27 +1,32 @@
-RSpec.describe 'POST /api/analyses', type: :request do
+RSpec.describe "POST /api/analyses", type: :request do
   let(:expected_response) do
-    file_fixture(image_response.json).read
+    file_fixture("image_response.json").read
   end
-
-  params = { analysis: {
-    resource: 'https://www.momentsandthings.se/wp-content/uploads/2016/06/Gunga-ek-med-namn.jpg',
-    category: 'image'
-  } }
-
+  
+  let(:params) do
+    params = { analysis: {
+      resource: 'https://www.momentsandthings.se/wp-content/uploads/2016/06/Gunga-ek-med-namn.jpg',
+      category: 'image'
+      }}
+  end
+ 
   subject { response }
 
   describe 'when the image api is used' do
     before do
       stub_request(:post,
-                   'https://safe-for-work-api.herokuapp.com/api/analyses').to_return(
-                     body: expected_response
-                   )
+                   "https://safe-for-work-api.herokuapp.com/api/analyses").to_return(
+        body: expected_response,
+      )
+      post "/api/analyses", params: params
     end
 
-    it 'is expected to have a category of image' do
-      expect(response_json['category']).to eq 'image'
+    it "is expected to have a category of image" do
+      expect(response_json["category"]).to eq "image"
     end
   end
+
+end
 
   # describe 'when there are no images in the database' do
   #   it { is_expected.to have_http_status 200 }
@@ -31,4 +36,3 @@ RSpec.describe 'POST /api/analyses', type: :request do
   #     expect(response_json['message']).to eq 'There are no images'
   #   end
   # end
-end
