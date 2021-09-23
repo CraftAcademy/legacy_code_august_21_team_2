8868f2ca93856e3c9316a82b63f34a6797177644
Fraser <Fraser.Hughes@btinternet.com>
2021-09-23 21:39:56 +0200

    Added alternative response from MonkeyLearn api and added more to text_spec rb

--- spec/requests/api/text_spec.rb
+++ spec/requests/api/text_spec.rb
@@ -1,11 +1,28 @@
 RSpec.describe 'POST /api/analyses', type: :request do
-   describe 'when the text api is used' do
-    before do
-     stub_request('https://api.monkeylearn.com/v3/classifiers/cl_KFXhoTdt/classify/')
-     .with(body: 'I fucking love eating corndogs').to_return(status: 200, body: expected_response)
-     post '/api/analyses', params: params
-    end
-   end
+  let(:expect_response) do
+    JSON.parse(file_fixture('text_resonse.json').read)
+  end
 
+  let(:params) do
+    { analysis: {
+      resource: 'I fucking love eating corndogs',
+      category: 'text'
+    } }
+  end
+
+  describe 'when the text api is used' do
+    before do
+      stub_request(:post, 'https://api.monkeylearn.com/v3/classifiers/cl_KFXhoTdt/classify/')
+        .with(body: 'I fucking love eating corndogs').
+        to_return(status: 200, body: expected_response)
+      post '/api/analyses', params: params
+    end
+  end
+
+  it { is_expected.to have_http_status 200 }
+
+  it 'is expected to have a category of text' do
+    #binding.pry
+    expect(response_json['category']).to eq 'text'
+  end
 end
-  
